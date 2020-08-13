47f31e2a523816b8e45e23dfbf3a382529cd541d
DDA209 <didier.jeux1@gmail.com>
2020-08-13 18:31:08 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import{
+  View,
+  Text,
+  StyleSheet,
+  ScrollView
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  render(){
+    return(
+      <ScrollView style={styles.container}>
+        <Text style={styles.title}> 
+          Marche à suivre
+        </Text>
+        <Text style={styles.paragraph}>
+          Un RÊVE écrit avec une date devient un OBJECTIF.
+        </Text>
+        <Text style={styles.paragraph}>
+          Un OBJECTIF décomposé en plusieurs étapes devient un PLAN.
+        </Text>
+        <Text style={styles.paragraph}>
+          Un PLAN soutenu par des actions devient
+        </Text>
+        <Text style={styles.paragraph}>
+          REALITE.
+        </Text>
+        <Text style={styles.signature}>
+          Auteur inconnu.
+        </Text>
+      </ScrollView>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    backgroundColor: '#ffffd0ff'
   },
+  textColor: {
+    color: '#402000ff',
+  },
+  title: {
+    fontSize: 30,
+    paddingVertical: 80
+  },
+  paragraph: {
+    textAlign : 'center'
+  },
+  signature: {
+    fontWeight: 'bold',
+    paddingVertical: 80
+  }
 });
+
+export default App;
